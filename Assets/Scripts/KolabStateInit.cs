b0379fdf0d33883dfbf63ef41b49dbd9481ed657
Florian Geiselhart <florian.geiselhart@uni-ulm.de>
2019-03-28 16:43:32 +0100

    Add' to prev commit

---
+++ Assets/Scripts/KolabStateInit.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KolabStateInit : MonoBehaviour
+{
+    public GameObject[] internalRenderObjs;
+    public GameObject[] externalRenderObjs;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Create a Kolab instance parametrized by the Environmental Constants
+        if (EnvConstants.ExternalRendererMode == false)
+        {
+            foreach (GameObject go in externalRenderObjs)
+            {
+                go.SetActive(false);
+                Debug.Log("Deactivating " + go.name);
+            }
+
+            foreach (GameObject go in internalRenderObjs)
+            {
+                go.SetActive(true);
+                Debug.Log("Activating " + go.name);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //do nothing in here!
+    }
+}
