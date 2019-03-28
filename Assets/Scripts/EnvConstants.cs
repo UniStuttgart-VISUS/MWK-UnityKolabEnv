7b111af831ffa94e7376fb2a9cb257a5d93843ed
Florian Geiselhart <florian.geiselhart@uni-ulm.de>
2019-03-28 16:42:37 +0100

    Extended dummy molecule PDB loader for IxD

--- Assets/Scripts/EnvConstants.cs
+++ Assets/Scripts/EnvConstants.cs
@@ -2,9 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class EnvConstants
+public class EnvConstants : MonoBehaviour
 {
-    public const bool UseInviwoPositioning = true;
-    public const bool RttVisualization = false;
-    public const bool CreateRoomOnLoad = false;
+
+    private static EnvConstants e_Instance = null; 
+ 
+    public static EnvConstants instance
+    {
+        get
+        {
+            if (e_Instance == null)
+            {
+                // FindObjectOfType() returns the first AManager object in the scene.
+                e_Instance = FindObjectOfType(typeof(EnvConstants)) as EnvConstants;
+            }
+ 
+            if (e_Instance == null)
+            {
+                var obj = new GameObject("AManager");
+                e_Instance = obj.AddComponent<EnvConstants>();
+            }
+ 
+            return e_Instance;
+        }
+    }
+
+    //Here comes our actual env values (both prop and field)
+    
+    public static bool UseInviwoPositioning
+    {
+        get { return instance._useInviwoPositioning; }
+        set { instance._useInviwoPositioning = value; }
+    }
+    
+    public static bool RttVisualization
+    {
+        get { return instance._rttVisualization; }
+        set { instance._rttVisualization = value; }
+    }
+    
+    public static bool CreateRoomOnLoad
+    {
+        get { return instance._createRoomOnLoad; }
+        set { instance._createRoomOnLoad = value; }
+    }
+    
+    public static bool AutoJoinFirstRoomOnLoad
+    {
+        get { return instance._autoJoinFirstRoomOnLoad; }
+        set { instance._autoJoinFirstRoomOnLoad = value; }
+    }
+
+    public static bool ExternalRendererMode
+    {
+        get { return instance._externalRendererMode; }
+        set { instance._externalRendererMode = value; }
+    }
+ 
+    public bool _useInviwoPositioning = true;
+    public bool _rttVisualization = false;
+    public bool _createRoomOnLoad = false;
+    public bool _autoJoinFirstRoomOnLoad = true;
+    public bool _externalRendererMode = false;
 }
