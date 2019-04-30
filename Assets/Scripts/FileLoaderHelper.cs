3e3eeffcdb405e810188de463836ee886102b70b
Florian Geiselhart <florian.geiselhart@uni-ulm.de>
2019-04-30 16:11:22 +0200

    * Added support for inviwo remote start, inv Workspace Loading (for thumbnail images) * Added preliminary loader prototype, first working proto * Added render manager script to configure textures / adaptive resolution (not yet working completely)

---
+++ Assets/Scripts/FileLoaderHelper.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class FileLoaderHelper : MonoBehaviour
+{
+    public List<string> availableFiles = new List<string>();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Scan working dir
+        var info = new DirectoryInfo("C:\\Users\\flo\\Documents\\KolabWorking\\inviwo\\");
+        var fileInfo = info.GetFiles("*.inv");
+        foreach (var file in fileInfo)
+        {
+            availableFiles.Add(file.FullName);
+        }
+        
+        //Create objects per file
+        for (int i = 0; i < availableFiles.Count; i++)
+        {
+            // Try read thumbnail from file
+            Texture2D tex = parseXmlFileReadThumb(File.ReadAllText(availableFiles[i]));
+            
+            // Create obj
+            var square = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            square.transform.parent = this.transform;
+            square.transform.localPosition = new Vector3(-1.2f,0.2f,i%5*0.1f);
+            square.transform.localScale = new Vector3(0.2f,0.2f,0.01f);
+            square.GetComponent<Renderer>().material = new Material(Shader.Find("Unlit/Texture"));
+            square.GetComponent<Renderer>().material.mainTexture = tex;
+            square.transform.RotateAround(square.transform.position, square.transform.right, 180f);
+            square.AddComponent<Button>().name = availableFiles[i];
+            square.GetComponent<Button>().onClick.AddListener(OnClick);
+        }
+    }
+
+    private void OnClick()
+    {
+        Debug.Log(EventSystem.current.currentSelectedGameObject.name);
+        ExternalApplicationController.Instance.StartInviwoInstance(EventSystem.current.currentSelectedGameObject.name);
+    }
+
+    Texture2D parseXmlFileReadThumb(string xmlData){
+        string totVal = "";
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.Load (new StringReader(xmlData));
+        string xmlPathPattern = "//CanvasImage/base64/@content";
+        XmlNode canvasNode  = xmlDoc.SelectSingleNode(xmlPathPattern);
+
+        Texture2D tex = new Texture2D(512, 512);
+        if (canvasNode != null)
+        {
+            //Create texture
+            byte[] b64_bytes = System.Convert.FromBase64String(canvasNode.Value);
+            tex.LoadImage(b64_bytes);
+        }
+
+        return tex;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/SendAll.cs
+++ Assets/Scripts/SendAll.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Globalization;
 using System.Text;
 using UnityEngine;
 using NetMQ;
 using NetMQ.Sockets;
 using UnityEngine.XR;
+using Valve.Newtonsoft.Json;
 using Valve.VR;
 
 public class SendAll : MonoBehaviour
@@ -17,63 +19,13 @@ public class SendAll : MonoBehaviour
     public float multiplier = 1.0f;
     public GameObject localRttMatGO;
     public Vector3 offsetY;
+    private float fovFade = 90;
+    private Vector2Int t = new Vector2Int(100,100);
     
     // Start is called before the first frame update
     void Start()
     {
-        if(baseCamera == null) baseCamera = Camera.main;
-
-        // Setup render values
-        uint w = 0, h = 0;
-        OpenVR.System.GetRecommendedRenderTargetSize(ref w, ref h);
-        var sceneWidth = (float)w;
-        var sceneHeight = (float)h;
-        
-        float l_left = 0.0f, l_right = 0.0f, l_top = 0.0f, l_bottom = 0.0f;
-        OpenVR.System.GetProjectionRaw(EVREye.Eye_Left, ref l_left, ref l_right, ref l_top, ref l_bottom);
-        
-        
-        float r_left = 0.0f, r_right = 0.0f, r_top = 0.0f, r_bottom = 0.0f;
-        OpenVR.System.GetProjectionRaw(EVREye.Eye_Right, ref r_left, ref r_right, ref r_top, ref r_bottom);
-        
-        var tanHalfFov = new Vector2(
-            Mathf.Max(-l_left, l_right, -r_left, r_right),
-            Mathf.Max(-l_top, l_bottom, -r_top, r_bottom));
-
-        var textureBounds = new VRTextureBounds_t[2];
-
-        textureBounds[0].uMin = 0.5f + 0.5f * l_left / tanHalfFov.x;
-        textureBounds[0].uMax = 0.5f + 0.5f * l_right / tanHalfFov.x;
-        textureBounds[0].vMin = 0.5f - 0.5f * l_bottom / tanHalfFov.y;
-        textureBounds[0].vMax = 0.5f - 0.5f * l_top / tanHalfFov.y;
-
-        textureBounds[1].uMin = 0.5f + 0.5f * r_left / tanHalfFov.x;
-        textureBounds[1].uMax = 0.5f + 0.5f * r_right / tanHalfFov.x;
-        textureBounds[1].vMin = 0.5f - 0.5f * r_bottom / tanHalfFov.y;
-        textureBounds[1].vMax = 0.5f - 0.5f * r_top / tanHalfFov.y;
-        
-        // Grow the recommended size to account for the overlapping fov
-        sceneWidth = sceneWidth / Mathf.Max(textureBounds[0].uMax - textureBounds[0].uMin, textureBounds[1].uMax - textureBounds[1].uMin);
-        sceneHeight = sceneHeight / Mathf.Max(textureBounds[0].vMax - textureBounds[0].vMin, textureBounds[1].vMax - textureBounds[1].vMin);
-
-        var aspect = tanHalfFov.x / tanHalfFov.y;
-        var fieldOfView = 2.0f * Mathf.Atan(tanHalfFov.y) * Mathf.Rad2Deg;
-
-        Debug.LogWarning("Tex size recommended:" + sceneWidth + " by " + sceneHeight + " px");
-        Debug.LogWarning("Aspect: " +aspect);
-        Debug.LogWarning("FoV: " +fieldOfView);
-
-        for (int i = 0; i < 2; i++)
-        {
-            Debug.LogWarning("TexBounds "+i+" uMin: "+textureBounds[i].uMin.ToString("f4"));
-            Debug.LogWarning("TexBounds "+i+" Left "+(sceneWidth-(sceneWidth*textureBounds[i].uMin))+" px");
-            Debug.LogWarning("TexBounds "+i+" uMax: "+textureBounds[i].uMax.ToString("f4"));
-            Debug.LogWarning("TexBounds "+i+" Right "+(sceneWidth-(sceneWidth*textureBounds[i].uMax))+" px");
-            Debug.LogWarning("TexBounds "+i+" vMin: "+textureBounds[i].vMin.ToString("f4"));
-            Debug.LogWarning("TexBounds "+i+" Top "+(sceneHeight-(sceneHeight*textureBounds[i].vMin))+" px");
-            Debug.LogWarning("TexBounds "+i+" vMax: "+textureBounds[i].vMax.ToString("f4"));
-            Debug.LogWarning("TexBounds "+i+" Bottom "+(sceneHeight-(sceneHeight*textureBounds[i].vMax))+" px");
-        }
+        if(baseCamera == null) baseCamera = Camera.main;       
     }
 
     // Update is called once per frame
@@ -113,8 +65,8 @@ public class SendAll : MonoBehaviour
 
         if (count % 2 == 0 || true)
         {
+            //Send camera continuously
             string jsonSend = "{" +
-                                  "\"seqNum\":" + count + "," +
                                   "\"camVecCamL\":" + JsonUtility.ToJson(vec1) + "," +
                                   "\"camVecCamR\":" + JsonUtility.ToJson(vec2) + "," +
                                   "\"camUpCamL\":" + JsonUtility.ToJson(vecUp) + "," +
@@ -123,6 +75,11 @@ public class SendAll : MonoBehaviour
                                   "\"camFwdCamR\":" + JsonUtility.ToJson(vecFwd) +
                               "}";
             _pubSocket.SendMoreFrame("camera").SendFrame(jsonSend);
+            
+            //Test/Sample: Send fov once, defined as interaction property in inviwo
+//            jsonSend = "{\"value\":"+JsonConvert.SerializeObject(t)+ " }";
+//            _pubSocket.SendMoreFrame("res").SendFrame(jsonSend);
+//            Debug.Log(jsonSend);
         }
 
         if (localRttMatGO != null)
