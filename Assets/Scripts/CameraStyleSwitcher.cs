2b72dd8b487cbbb6e61c037458f9593ef218953f
Florian Geiselhart <florian.geiselhart@uni-ulm.de>
2019-05-09 14:17:51 +0200

    CameraStyleSwitcher added (anon mode preparations)

---
+++ Assets/Scripts/CameraStyleSwitcher.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraStyleSwitcher : MonoBehaviour {
+
+    public Material material;
+
+    void OnRenderImage (RenderTexture source, RenderTexture destination) {
+        Graphics.Blit(source, destination, material);
+    }
+}
