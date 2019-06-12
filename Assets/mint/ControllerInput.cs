f8a76fde4b14654da203af6aeceb09bb10816c0a
Sergej Geringer <sergej.geringer@visus.uni-stuttgart.de>
2019-06-12 21:05:51 +0200

    add experimental controller interaction

---
+++ Assets/mint/ControllerInput.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Photon.Pun;
+
+public class ControllerInput : MonoBehaviour {
+
+    protected GameObject collidingObject = null;
+    protected GameObject objectInHand = null;
+
+    private GameObject proxy;
+
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Action_Boolean grabActiveAction;
+
+    bool logging = false;
+
+    void log(string s){
+        if(logging)
+            print(s);
+    }
+
+	// Use this for initialization
+	void Start () {
+        proxy = new GameObject("GrabProxyObject");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        proxy.transform.SetPositionAndRotation(this.transform.position, this.transform.rotation);
+
+        if (grabActiveAction.GetLastStateDown(handType) && collidingObject)
+        {
+            GrabObject();
+            Debug.Log("picked up object");
+        }
+
+        if (grabActiveAction.GetLastStateUp(handType) && objectInHand)
+        {
+            ReleaseObject();
+            Debug.Log("picked up object");
+        }
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        log("OnTriggerEnter");
+        SetCollidingObject(other);
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        log("OnTriggerStay");
+        SetCollidingObject(other);
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        log("OnTriggerExit");
+        RemoveCollidingObject(other);
+    }
+
+    public void SetCollidingObject(Collider other)
+    {
+        if (collidingObject || !other.GetComponent<Rigidbody>())
+            return;
+
+        collidingObject = other.gameObject;
+    }
+
+    public void RemoveCollidingObject(Collider other)
+    {
+        if (!collidingObject)
+            return;
+
+        collidingObject = null;
+    }
+
+    public void GrabObject()
+    {
+        if (!collidingObject)
+            return;
+        
+        if(!collidingObject.GetComponent<PhotonView>().IsMine)
+            collidingObject.GetComponent<PhotonView>().TransferOwnership(PhotonNetwork.LocalPlayer);
+
+        objectInHand = collidingObject;        
+        var io = objectInHand.GetComponent<InteractiveObject>();
+        if(io)
+            io.Pickup(proxy);
+    }
+
+    public void ReleaseObject()
+    {
+        if (!objectInHand)
+            return;
+
+        objectInHand.GetComponent<PhotonView>().TransferOwnership(0);
+
+        var io = objectInHand.GetComponent<InteractiveObject>();
+        if(io)
+            io.Release(proxy);
+    }
+}
---
+++ Assets/mint/InteractiveObject.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+[RequireComponent(typeof(Rigidbody))]
+public class InteractiveObject : MonoBehaviour {
+
+    public Camera hmdCamera;
+
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Action_Boolean rotationActiveAction;
+    public SteamVR_Action_Vector2 rotationDirectionAction;
+
+    public SteamVR_Action_Boolean scalingActiveAction;
+    public SteamVR_Action_Vector2 scalingValueAction;
+
+    // grabbing object
+    public SteamVR_Action_Boolean grabActiveAction;
+    protected Rigidbody rigidBody;
+    protected bool originalKinematicState;
+    protected Transform originalParent;
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        originalParent = transform.parent;
+        originalKinematicState = rigidBody.isKinematic;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (GetRotationActiveDown())
+        {
+            var rot = GetRotationDirection();
+            rot.x = Mathf.Abs(rot.x) < 0.5 ? 0.0f : rot.x;
+            rot.y = Mathf.Abs(rot.y) < 0.5 ? 0.0f : rot.y;
+            //this.transform.Rotate(new Vector3(rot.y, -rot.x, 0.0f), Space.World);
+
+            Vector3 rotAxisX = hmdCamera.transform.up;
+            Vector3 rotAxisY = hmdCamera.transform.right;
+            this.transform.RotateAround(this.transform.position, rotAxisX, -rot.x);
+            this.transform.RotateAround(this.transform.position, rotAxisY, rot.y);
+
+            return;
+        }
+
+        if(GetScalingActiveDown())
+        {
+            var scale = GetScalingValue();
+            if(scale != 0.0f)
+                this.transform.localScale = this.transform.localScale * ((scale < 0.0f) ? (0.99f) : (1.01f));
+            return;
+        }
+
+        if(GetGrabActiveDown())
+        {
+            return;
+        }
+	}
+
+    public bool GetRotationActiveDown()
+    {
+        return rotationActiveAction.GetState(handType);
+        //return rotationActiveAction.GetStateDown(handType);
+    }
+
+    public Vector2 GetRotationDirection()
+    {
+        return rotationDirectionAction.GetAxis(handType);
+        //return rotationDirectionAction.GetAxisDelta(handType);
+    }
+
+    public bool GetScalingActiveDown()
+    {
+        return scalingActiveAction.GetState(handType);
+    }
+
+    public float GetScalingValue()
+    {
+        return scalingValueAction.GetAxis(handType).y;
+    }
+
+    public bool GetGrabActiveDown()
+    {
+        return grabActiveAction.GetState(handType);
+    }
+
+    public void Pickup(GameObject holder)
+    {
+        rigidBody.isKinematic = true;
+
+        transform.SetParent(holder.transform);
+    }
+
+    public void Release(GameObject holder)
+    {
+        if(transform.parent == holder.transform)
+        {
+            rigidBody.isKinematic = originalKinematicState;
+
+            if(originalParent != holder.transform)
+            {
+                transform.SetParent(originalParent);
+            }
+            else
+            {
+                transform.SetParent(null);
+            }
+        }
+    }
+}
