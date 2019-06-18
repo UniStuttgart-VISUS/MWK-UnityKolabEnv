055440de0a5eda5bdceb4898ccdaa9c59a16c3c4
Florian Geiselhart <florian.geiselhart@uni-ulm.de>
2019-06-18 12:48:45 +0200

    Screenshot script migrated, temporary on floor...

---
+++ Assets/Scripts/ScreenshotInteraction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenshotInteraction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            print("space key was pressed");
+            ScreenCapture.CaptureScreenshot(Application.persistentDataPath+"/screenshot_"+DateTime.Now.ToString("yyyyMMddHHmmssfff")+".png",2);
+        }
+    }
+}
