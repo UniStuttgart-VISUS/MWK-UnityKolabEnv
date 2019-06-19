c0749b18d30590639b48166ae216baadd483dbc4
Florian Geiselhart <florian.geiselhart@uni-ulm.de>
2019-06-20 01:43:22 +0200

    3D Model + Animations for engineering use case (when externalRenderer off)

---
+++ Assets/Scripts/AnimTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HTC.UnityPlugin.Vive;
+using UnityEngine;
+
+public class TestAnimtrigger : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ViveInput.GetPressDownEx(HandRole.RightHand, ControllerButton.DPadUp))
+        {
+            Debug.Log("Spinning");
+            anim.Play("ExplodeAnimation");
+        }
+        
+        if (ViveInput.GetPressDownEx(HandRole.RightHand, ControllerButton.DPadDown))
+        {
+            Debug.Log("Spinning");
+            anim.Play("Unexplode");
+        }
+    }
+}
