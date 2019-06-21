163c93a7ebf9b959d0f282c3c09198af5d546a0b
Florian Geiselhart <florian.geiselhart@uni-ulm.de>
2019-06-22 01:15:43 +0200

    Cutting Plane stuff, smaller fixes

--- Assets/Scripts/CuttingPlanePointSender.cs
+++ Assets/Scripts/CuttingPlanePointSender.cs
@@ -8,58 +8,23 @@ using Photon.Pun;
 using UnityEngine;
 using Valve.Newtonsoft.Json;
 
-[RequireComponent(typeof(PhotonView))]
-public class CuttingPlanePointSender : MonoBehaviourPun, IPunObservable, IJsonStringSendable {
+public class CuttingPlanePointSender : MonoBehaviour, IJsonStringSendable {
 
-    public bool isCutting = false;
     public string Name = "CuttingPlanePoint";
-    private Vector4 lastSentValue = new Vector4();
+    private Vector3 lastSentValue = new Vector4();
+    private CuttingPlaneStateSender stateSender;
+    public GameObject objectToCut;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        stateSender = GetComponent<CuttingPlaneStateSender>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Input
-        if(!ViveInput.Active) Debug.LogWarning("ViveInput inactive");
-            
-        if (ViveInput.GetPressDownEx(HandRole.LeftHand, ControllerButton.Grip) && !isCutting)
-        {
-            Debug.Log("Start cut");
-            //start grab            
-            isCutting = true;
-            Vector3 currentPos = VivePose.GetPoseEx(HandRole.LeftHand).pos;
-            Vector3 currentVec = VivePose.GetPoseEx(HandRole.LeftHand).rot * VivePose.GetPoseEx(HandRole.LeftHand).up;
-            //photon take ownership if necessary
-            if (!this.photonView.IsMine)
-            {
-                this.GetComponent<PhotonView>().TransferOwnership(PhotonNetwork.LocalPlayer);
-            }
-            
-        } else if (ViveInput.GetPressUpEx(HandRole.LeftHand, ControllerButton.Grip) && isCutting) {
-            //end grab
-            Debug.Log("End cut");
-            this.GetComponent<PhotonView>().TransferOwnership(0);
-            isCutting = false;
-        }
-        else if(isCutting)
-        {
-            Vector3 currentPos = VivePose.GetPoseEx(HandRole.LeftHand).pos;
-            Vector3 currentVec = VivePose.GetPoseEx(HandRole.LeftHand).rot * VivePose.GetPoseEx(HandRole.LeftHand).up;
-        }
-
-        //only use values from network if not grabbed locally
-        if (!isCutting && !this.GetComponent<PhotonView>().IsMine)
-        {
-            Debug.Log("Update cut remote");
-            //Add incoming network translation / rotation (not really accounting for collisions or concurrency right now)
-            //transform.position = Vector3.Lerp(transform.position, latestPos, Time.deltaTime * 2);
-            //this.transform.rotation = Quaternion.Lerp(transform.rotation, latestRot, Time.deltaTime * 2);
-        }
+       
     }
     
     public string nameString() {
@@ -68,18 +33,13 @@ public class CuttingPlanePointSender : MonoBehaviourPun, IPunObservable, IJsonSt
 
     public string jsonString()
     {
-        Vector4 currentVec = transform.rotation * Vector3.down;
+        Vector3 currentVec = -objectToCut.transform.InverseTransformPoint(transform.position);
         lastSentValue = currentVec;
         return "{\"value\":"+JsonUtility.ToJson(currentVec)+ " }";
     }
 
     public bool hasChanged()
     {
-        return lastSentValue != (Vector4)(transform.rotation * Vector3.down);
-    }
-
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    {
-        //nothing yet
+        return stateSender.lastSentValue;
     }
 }
