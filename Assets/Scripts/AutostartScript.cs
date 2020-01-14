6a71940d84a83546ba81e180becbbbb76b3e71a2
Sergej Geringer <sergej.geringer@visus.uni-stuttgart.de>
2020-01-14 15:51:28 +0100

    use Autostart object spawned in all scenes for global parameters

---
+++ Assets/Scripts/AutostartScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutostartScript : MonoBehaviour
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    public static void LoadAutostart() {
+        GameObject autostart = GameObject.Instantiate(Resources.Load("Autostart")) as GameObject;
+        GameObject.DontDestroyOnLoad(autostart);
+    }
+}
