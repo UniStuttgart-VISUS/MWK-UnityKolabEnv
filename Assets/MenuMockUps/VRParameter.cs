6f70ef28cfbbfb5fe7f7e5c79d770d0b727e8462
FelixBelz <33028665+FelixBelz@users.noreply.github.com>
2020-01-23 21:27:17 +0100

    Fixed missing prefabs bug

---
+++ Assets/MenuMockUps/VRParameter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using HTC.UnityPlugin.Vive;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class VRParameter : MonoBehaviour, IPointerClickHandler
+{
+    public OverlayMenu overlayMenu;
+    private CheckboxScript checkbox;
+    private PopUpMenuIcon Icon;
+    bool isActive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        checkbox = overlayMenu.GetInteraction();
+        Icon = overlayMenu.GetPopUpMenuIcon();
+        isActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        overlayMenu.Disappear();
+        Icon.Appear();
+        checkbox.Appear();
+    }
+}
