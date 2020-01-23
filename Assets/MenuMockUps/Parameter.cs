5fce12aa275e7ee1312c465eafbefbc99f464aa7
FelixBelz <33028665+FelixBelz@users.noreply.github.com>
2020-01-23 23:30:23 +0100

    Added taskbar menu mock up

---
+++ Assets/MenuMockUps/Parameter.cs
@@ -0,0 +1,27 @@
+﻿using HTC.UnityPlugin.Vive;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Parameter : MonoBehaviour, IPointerClickHandler
+{
+    public OwnTaskbarMenu Menu;
+    public OwnTaskbarMenuItem item;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Menu.add(item);
+    }
+}
